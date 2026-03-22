64ef93cb6019a26b481b0804f01fe427826401ef
dakerman <a.daniel.akerman@gmail.com>
2026-03-22 13:30:37 +0100

    add HQ bluetooth audio

--- modules/system/bluetooth/default.nix
+++ modules/system/bluetooth/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 
 {
@@ -11,5 +11,28 @@ with lib;
       enable = true;
       powerOnBoot = true;
     };
+
+    # Enable high-quality Bluetooth audio codecs via Wireplumber
+    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.codecs" = [
+          "sbc"
+          "sbc_xq"
+          "aac"
+          "ldac"
+          "aptx"
+          "aptx_hd"
+          "aptx_ll"
+          "aptx_ll_duplex"
+          "aptx_adaptive"
+        ];
+      };
+    };
+
+    # aptX codec support library
+    environment.systemPackages = [ pkgs.libfreeaptx ];
   };
 }
